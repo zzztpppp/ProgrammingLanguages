38f02042fd36d98cb78e00882f38790ea969d8bd
zutianpei <zutianpei@linezonedata.com>
2020-01-04 18:57:36 +0800

    Problem 4 added

--- hw4.rkt
+++ hw4.rkt
@@ -12,15 +12,33 @@
       null
       (cons low (sequence (+ low stride) high stride))))
 
+
 ;problem 2
 (define (string-append-map xs suffix)
   (map (lambda (x) (string-append x suffix)) xs))
 
+
 ;problem 3
 (define (list-nth-mod xs n)
-  (cond [(<0 n) (error "list-nth-mod: negative number")]
+  (cond [(< n 0) (error "list-nth-mod: negative number")]
         [(null? xs) (error "list-nth-mod: empty list")]
-        [#t (let l 
-  
+        [#t (let* ([l (length xs)]
+                  [r (remainder n l)])
+              (car (list-tail xs r)))]))
+
+
+;problem 4
+(define (stream-for-n-steps s n)
+  (if (> n 0)
+      (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))
+      null))
+
+
+;problem 5
+(define funny-number-stream
+  (letrec ([f (lambda (x) (if (= (remainder x 5) 0)
+                              (cons (- x) (lambda()(f ( + (1 x)))))
+                              (cons x (lambda () (f ( + (1 x)))))))])
+           (lambda () (f 1))))
 
 
